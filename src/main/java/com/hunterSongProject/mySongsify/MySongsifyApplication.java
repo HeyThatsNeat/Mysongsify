b1c4325f518ef7ff925d7329d56583dddc73054c
Hunter Eckhoff <Phantazm777@verizon.net>
2024-01-17 11:58:12 -0500

    initialize project

---
+++ src/main/java/com/hunterSongProject/mySongsify/MySongsifyApplication.java
@@ -0,0 +1,13 @@
+package com.hunterSongProject.mySongsify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySongsifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySongsifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hunterSongProject/mySongsify/MySongsifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunterSongProject.mySongsify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySongsifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
